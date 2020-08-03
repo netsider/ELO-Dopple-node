22bd9de72e1ad985d0aad9a39015c613c2ac2cb0
Russell R <rdoubleoc@aol.com>
2020-08-03 02:05:01 -0400

    Wing

--- non-dopple-api/index.js
+++ non-dopple-api/index.js
@@ -31,3 +31,15 @@ app.post("/submitPlayer", (req, res, next) => {
 	res.json(newObj);
 	// res.json(req.body);
 });
+
+
+
+function ELO(A, B){
+	return 1 / (1 + Math.pow(10,((B - A)/400)));
+};
+
+function getRandomIntInclusive(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+};
